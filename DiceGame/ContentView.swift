35e1b96b90f272a70491d24e15a6fec6b1dcd9ef
zachzhangx <101433206+zachzhangx@users.noreply.github.com>
2022-03-12 23:08:33 -0800

    Initial Commit

---
+++ DiceGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceGame
+//
+//  Created by Zhang Xu on 2022/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Zhang Xu on 2022/3/12.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
