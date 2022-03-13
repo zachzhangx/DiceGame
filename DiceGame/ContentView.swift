ef1b705be175f5c0d072c977909ecf321428ac39
zachzhangx <101433206+zachzhangx@users.noreply.github.com>
2022-03-13 00:42:57 -0800

    DiceGame SwiftUI Practise

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftNumber = 1
+    @State var rightNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: leftNumber)
+                    DiceView(number: rightNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action:{
+                    self.leftNumber = Int.random(in: 1...6)
+                    self.rightNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size:40))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(.red)
+                .cornerRadius(10)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let number : Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
